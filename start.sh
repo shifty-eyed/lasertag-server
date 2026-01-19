e43e742bbcbe33e6ab0c77b182a58e4cef3457c6
shyfty-eyed <nifect@gmail.com>
2026-01-19 14:12:20 -0500

    start script fix

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Define the main class
-MAIN_CLASS="net.lasertag.lasertagserver.LasertagServerApplication"
-
-# Build the classpath by including all jars from ./lib and ./target/classes
-CLASSPATH="target/classes:$(find lib -name "*.jar" | tr '\n' ':')"
-
-# Run the Java application
-java -cp "$CLASSPATH" "$MAIN_CLASS"
-
+# Run the Spring Boot application from the packaged JAR
+java -jar target/lasertag-server-0.0.1-SNAPSHOT.jar
